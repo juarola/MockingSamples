66f34f482ef33bc35829788fc52f456d248444e5
Jukka Arola <jukka.arola@gmail.com>
2013-11-08 18:04:37 +0200

    simple sample

---
+++ source/Mocking.Tests/MockingTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+
+using NSubstitute;
+
+using NUnit.Framework;
+
+namespace Mocking.Tests
+{
+    [TestFixture]
+    public class MockingTests
+    {
+        private Model _model;
+
+        [SetUp]
+        public void Setup()
+        {
+            _model = new Model { Name = "heps" };
+        }
+
+        [Test]
+        public void Test()
+        {
+            var repo = new Repository();
+
+            var service = new Service(repo);
+
+            service.MethodToTest(_model);
+        }
+        
+        [Test]
+        public void TestNsubstitute()
+        {
+            var mockRepo = Substitute.For<IRepository>();
+            
+            var service = new Service(mockRepo);
+
+            service.MethodToTest(_model);
+
+            mockRepo.Received().Save(Arg.Is<DTO>(x => x.Name == _model.Name));
+        }
+
+        [Test]
+        public void TestNsubstituteComplex()
+        {
+            var mockRepo = Substitute.For<IRepository>();
+
+            var service = new Service(mockRepo);
+
+            service.MethodToTest(_model);
+
+            mockRepo.Received().Save(Arg.Is<DTO>((dto) => EqualityTest(dto)));
+        }
+
+
+        [Test]
+        public void TestMoq()
+        {
+            var mockRepo = new Mock<IRepository>();
+
+            mockRepo.Setup(x => x.Save(It.IsAny<DTO>()));
+
+            var service = new Service(mockRepo.Object);
+
+            service.MethodToTest(_model);
+
+            mockRepo.Verify(x => x.Save(It.Is<DTO>(y => y.Name == "hop")), Times.Once);
+        }
+
+        [Test]
+        public void TestMoqComplex()
+        {
+            var mockRepo = new Mock<IRepository>();
+
+            mockRepo.Setup(x => x.Save(It.IsAny<DTO>()));
+
+            var service = new Service(mockRepo.Object);
+
+            service.MethodToTest(_model);
+
+            mockRepo.Verify(x => x.Save(It.Is<DTO>((dto)=>EqualityTest(dto))));
+        }
+
+        private bool EqualityTest(DTO dto)
+        {
+            if(dto.Name.Equals(_model.Name))
+                return true;
+            
+            return false;
+        }
+    }
+}
---
+++ source/Mocking/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new Model();
+            model.Name = "heps";
+
+            var service = new Service();
+
+            service.MethodToTest(model);
+
+        }
+    }
+
+    public class Model
+    {
+        public string Name { get; set; }
+    }
+
+    public class DTO
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IService
+    {
+        void MethodToTest(Model model);
+    }
+
+    public class Service : IService
+    {
+        private IRepository _repository;
+
+        public Service()
+        {
+            this._repository = new Repository();
+        }
+
+        public Service(IRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public void MethodToTest(Model model)
+        {
+            var dto = new DTO();
+            dto.Name = model.Name;
+
+            _repository.Save(dto);
+        }
+    }
+
+    public interface IRepository
+    {
+        void Save(DTO dto);
+    }
+
+    public class Repository : IRepository
+    {
+        public void Save(DTO dto)
+        {
+            // do stuff
+        }
+    }
+}
